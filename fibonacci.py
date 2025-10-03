e7a421e8f29b0643cd7caa363653233ab81c4ecd
Miguel Sanchez Rolas <msanch06@uoguelph.ca>
2025-10-03 13:46:48 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -18,15 +18,15 @@ while not user_input.isdigit() or int(user_input) < 0:
 user_input = int(user_input)
 # Use a for loop to print the Fibonacci sequence up to that many terms.
 
-num = 0
-num_one = 1
-num_two = 0
-temp = 0
+num_one = 0
+num_two = 1
 
 
-for num in range(user_input):
-  print(num)
-  num = num_one + num_two
-  temp = num_one
-  num_one = num
-  num_two = num_one
+
+for i in range(user_input):
+    print(num_one, end='')
+    if i < user_input - 1:
+        print(' ', end='')
+    temp = num_one
+    num_one = num_two
+    num_two = temp + num_two
